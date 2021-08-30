de1944d6253087feb7682779a0eb056ec5e61ac0
samon03 <samon3123@gmail.com>
2021-08-30 20:59:29 +0600

    build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliry';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
 
 class BurgerBuilder extends Component {
 
@@ -16,7 +18,7 @@ class BurgerBuilder extends Component {
         return (
            <Aux>
                <Burger ingredients={this.state.ingredients}/>
-               <div>Burger Controls</div>
+               <BuildControls/>
            </Aux>
         );
     }
