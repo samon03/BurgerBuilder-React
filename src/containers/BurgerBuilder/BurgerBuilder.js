ff329dca5a18794c4e1f7899923e97350d17e36b
samon03 <samon3123@gmail.com>
2021-09-01 15:42:59 +0600

    removed ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,17 +31,46 @@ class BurgerBuilder extends Component {
 
         updateIngredients[type] = updateCount;
         const priceAddition = INGREDIENT_PRICE[type];
-        console.log(priceAddition);
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updateIngredients});
     }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0)
+        {
+           return;
+        }
+        const updateCount = oldCount - 1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        }
+
+        updateIngredients[type] = updateCount;
+        const priceDeduction = INGREDIENT_PRICE[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+    }
+
     render() {
+
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+
+        for(let key in disabledInfo)
+        {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
            <Aux>
                <Burger ingredients={this.state.ingredients}/>
                <BuildControls 
-                 ingredientAdded={this.addIngredientHandler}/>
+                 ingredientAdded={this.addIngredientHandler}
+                 ingredientRemoved={this.removeIngredientHandler}
+                 disabled={disabledInfo}/>
            </Aux>
         );
     }
