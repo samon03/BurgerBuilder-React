1a529e99ba769cec1b92b8710344cff45670a8d3
samon03 <samon3123@gmail.com>
2021-10-18 16:07:38 +0600

    sign in form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './components/Auth/Auth';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -18,6 +19,7 @@ class App extends Component {
            <Switch>
               <Route path="/checkout" component={Checkout}/>
               <Route path="/orders" component={Orders}/>
+              <Route path="/auth" component={Auth}/>
               <Route path="/" exact component={BurgerBuilder}/>
            </Switch>
          </BrowserRouter>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,7 @@ const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/">Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
+        <NavigationItem link="/auth">Auth</NavigationItem>
     </ul>
 );
 
